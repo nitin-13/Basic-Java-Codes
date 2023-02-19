58c9e0983a85af427d77577ff521d72cdd45d4d6
nitin-13 <64396511+nitin-13@users.noreply.github.com>
2023-02-19 21:19:00 +0530

    Add files via upload
    This Java program table a input number from user and print its table till multiple of 10

---
+++ Table.java
@@ -0,0 +1,23 @@
+package Practice;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Table {
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+         bufferedReader.close();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(N+" X "+i+" = "+(N*i));    
+        }
+    }
+	
+}
