8acba34b8f0da21d18c61f7aaea95aed1dcd6861
nitin-13 <shetye.nitin13@gmail.com>
2023-05-12 16:00:25 +0530

    New committed

---
+++ LinkedListExample.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class LinkedListExample {
+	public static void main() {
+		LinkedList<String> car = new LinkedList<String>();
+		
+		car.add("Maruti");
+		car.add("Hyundai");
+		car.add("Tata");
+		car.add("Toyota");
+		car.add("Mahindra");
+		
+		System.out.println(car);
+	}
+}
--- Table.java
+++ Table.java
@@ -1,4 +1,4 @@
-package Practice;
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
